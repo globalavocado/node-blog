299d49d76abd931e242cd398e8f0bfca2a19b23d
globalavocado <globalavocado@users.noreply.github.com>
2020-03-05 15:17:14 +0000

    server running

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Ciao mondo!'));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
