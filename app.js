0593bca4b2613d0b307600dc2322fd333e75a6aa
globalavocado <globalavocado@users.noreply.github.com>
2021-02-07 18:52:17 +0100

    blog content on front page, retrieved from database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -6,7 +7,7 @@ const MongoClient = require('mongodb').MongoClient
 
 var db
 
-MongoClient.connect('mongodb://localhost/', 
+MongoClient.connect('mongodb://localhost/test', 
     { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -18,11 +19,17 @@ MongoClient.connect('mongodb://localhost/',
         app.listen(port, () => console.log(`Server running on port ${port}`));
 })
 
+// cors
+app.use(cors());
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Ciao mondo e ciao tutti!'));
-
-app.get('/blogposts', (req, res) => {
-    console.log('ciaaaaaaaaaooooooo bella!')
-    res.render('index.ejs')
-})
+app.get('/', (req, res) => {
+    db.collection('blog').find().toArray(function(err, result) {
+        if (err) return console.log(err)
+        res.render('index.ejs', {posts: result})
+    })
+});
